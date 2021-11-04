03e6c6bc6aa940fa7d7d994ae9171375ba90e5bb
SwannHtetAung <swannhtetaung.ss@gmail.com>
2021-11-04 09:11:07 +0630

    add to cart process using hardevine and dynamic cart count qty

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -17,6 +17,7 @@
     <link rel="stylesheet" type="text/css" href="{{asset('assets/css/font-awesome.min.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('assets/css/bootstrap.min.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('assets/css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/flexslider.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('assets/css/chosen.min.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('assets/css/style.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('assets/css/color-01.css')}}">
@@ -53,20 +54,25 @@
                                 </li>
                                 <li class="menu-item lang-menu menu-item-has-children parent">
                                     <a title="English" href="#"><span class="img label-before"><img
-                                                src="assets/images/lang-en.png" alt="lang-en"></span>English<i
-                                            class="fa fa-angle-down" aria-hidden="true"></i></a>
+                                                src="{{asset('assets/images/lang-en.png')}}"
+                                                alt="lang-en"></span>English<i class="fa fa-angle-down"
+                                            aria-hidden="true"></i></a>
                                     <ul class="submenu lang">
                                         <li class="menu-item"><a title="hungary" href="#"><span
-                                                    class="img label-before"><img src="assets/images/lang-hun.png"
+                                                    class="img label-before"><img
+                                                        src="{{asset('assets/images/lang-hun.png')}}"
                                                         alt="lang-hun"></span>Hungary</a></li>
                                         <li class="menu-item"><a title="german" href="#"><span
-                                                    class="img label-before"><img src="assets/images/lang-ger.png"
+                                                    class="img label-before"><img
+                                                        src="{{asset('assets/images/lang-ger.png')}}"
                                                         alt="lang-ger"></span>German</a></li>
                                         <li class="menu-item"><a title="french" href="#"><span
-                                                    class="img label-before"><img src="assets/images/lang-fra.png"
+                                                    class="img label-before"><img
+                                                        src="{{asset('assets/images/lang-fra.png')}}"
                                                         alt="lang-fre"></span>French</a></li>
                                         <li class="menu-item"><a title="canada" href="#"><span
-                                                    class="img label-before"><img src="assets/images/lang-can.png"
+                                                    class="img label-before"><img
+                                                        src="{{asset('assets/images/lang-can.png')}}"
                                                         alt="lang-can"></span>Canada</a></li>
                                     </ul>
                                 </li>
@@ -196,7 +202,9 @@
                                 <a href="#" class="link-direction">
                                     <i class="fa fa-shopping-basket" aria-hidden="true"></i>
                                     <div class="left-info">
-                                        <span class="index">4 items</span>
+                                        @if(Cart::count() > 0)
+                                        <span class="index">{{Cart::count()}} items</span>
+                                        @endif
                                         <span class="title">CART</span>
                                     </div>
                                 </a>
@@ -408,7 +416,7 @@
                                 <h3 class="item-header">We Using Safe Payments:</h3>
                                 <div class="item-content">
                                     <div class="wrap-list-item wrap-gallery">
-                                        <img src="assets/images/payment.png" style="max-width: 260px;">
+                                        <img src="{{asset('assets/images/payment.png')}}" style="max-width: 260px;">
                                     </div>
                                 </div>
                             </div>
@@ -443,12 +451,13 @@
                                     <div class="wrap-list-item apps-list">
                                         <ul>
                                             <li><a href="#" class="link-to-item" title="our application on apple store">
-                                                    <figure><img src="assets/images/brands/apple-store.png"
+                                                    <figure><img src="{{asset('assets/images/brands/apple-store.png')}}"
                                                             alt="apple store" width="128" height="36"></figure>
                                                 </a></li>
                                             <li><a href="#" class="link-to-item"
                                                     title="our application on google play store">
-                                                    <figure><img src="assets/images/brands/google-play-store.png"
+                                                    <figure><img
+                                                            src="{{asset('assets/images/brands/google-play-store.png')}}"
                                                             alt="google play store" width="128" height="36"></figure>
                                                 </a></li>
                                         </ul>
