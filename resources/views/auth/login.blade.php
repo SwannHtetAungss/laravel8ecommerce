5305c09da1886e1a670ddea52a977f04b941bda7
SwannHtetAung <swannhtetaung.ss@gmail.com>
2021-12-17 11:31:49 +0630

    make the header functional

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -50,7 +50,7 @@
 </x-guest-layout>
 --}}
 
-<x-guest-layout>
+<x-base-layout>
     <!--main area-->
     <main id="main" class="main-site left-sidebar">
 
@@ -109,4 +109,4 @@
 
     </main>
     <!--main area-->
-</x-guest-layout>
+</x-base-layout>
