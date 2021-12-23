f5f71ec1ff6d96e9b2d2bab05d7970a412e010d2
SwannHtetAung <swannhtetaung.ss@gmail.com>
2021-12-23 11:05:43 +0630

    admin edit product gallery

--- app/Http/Livewire/Admin/AdminEditProductComponent.php
+++ app/Http/Livewire/Admin/AdminEditProductComponent.php
@@ -29,6 +29,9 @@ class AdminEditProductComponent extends Component
     public $newimage;
     public $product_id;
 
+    public $images;
+    public $newimages;
+
     public function mount($product_slug)
     {
         $product = Product::where('slug', $product_slug)->first();
@@ -44,6 +47,7 @@ class AdminEditProductComponent extends Component
         $this->featured = $product->featured;
         $this->quantity = $product->quantity;
         $this->image = $product->image;
+        $this->images = explode(",", $product->images);
         $this->category_id = $product->category_id;
         $this->product_id = $product->id;
     }
@@ -61,13 +65,17 @@ class AdminEditProductComponent extends Component
             'short_description' => 'required',
             'description' => 'required',
             'regular_price' => 'required|numeric',
-            // 'sale_price' => 'numeric',
+            'sale_price' => 'numeric',
             'SKU' => 'required',
             'stock_status' => 'required',
             'quantity' => 'required|numeric',
-            // 'newimage' => 'sometimes|mimes:jpg,jpeg,png',
             'category_id' => 'required'
         ]);
+        if ($this->newimage) {
+            $this->validateOnly($fields, [
+                'newimage' => 'sometimes|mimes:jpg,jpeg,png',
+            ]);
+        }
     }
 
     public function updateProduct()
@@ -82,9 +90,14 @@ class AdminEditProductComponent extends Component
             'SKU' => 'required',
             'stock_status' => 'required',
             'quantity' => 'required|numeric',
-            // 'newimage' => 'sometimes|mimes:jpg,jpeg,png',
             'category_id' => 'required'
         ]);
+
+        if ($this->newimage) {
+            $this->validate([
+                'newimage' => 'sometimes|mimes:jpg,jpeg,png',
+            ]);
+        }
 
         $product = Product::find($this->product_id);
         $product->name = $this->name;
@@ -99,9 +112,30 @@ class AdminEditProductComponent extends Component
         $product->quantity = $this->quantity;
 
         if ($this->newimage) {
+            unlink('assets/images/products' . '/' . $product->image);
             $imageName = Carbon::now()->timestamp . '.' . $this->newimage->extension();
             $this->newimage->storeAs('products', $imageName);
             $product->image = $imageName;
+        }
+
+        if ($this->newimages) {
+            if ($product->images) {
+                $images = explode(",", $product->images);
+                foreach ($images as $image) {
+                    if ($image) {
+                        unlink('assets/images/products' . '/' . $image);
+                    }
+                }
+            }
+
+            $imagesname = "";
+            foreach ($this->newimages as $key => $image) {
+                $imgName = Carbon::now()->timestamp . $key . '.' . $image->extension();
+                $image->storeAs('products', $imgName);
+                $imagesname = $imagesname . ',' . $imgName;
+            }
+
+            $product->images = $imagesname;
         }
 
         $product->category_id = $this->category_id;
